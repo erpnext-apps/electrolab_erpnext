5bf04eca10b0a7c719c9ad16957043334857b842
Mangesh-Khairnar <mkhairnar10@gmail.com>
2019-08-12 19:46:35 +0530

    feat: generate bank remittance file via payment order

--- electrolab_erpnext/hooks.py
+++ electrolab_erpnext/hooks.py
@@ -10,6 +10,7 @@ app_icon = "octicon octicon-file-directory"
 app_color = "grey"
 app_email = "info@frappe.io"
 app_license = "MIT"
+fixtures = ["Custom Field", "Custom Script"]
 
 # Includes in <head>
 # ------------------
